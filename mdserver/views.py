671f0860906704951b7984c873d7e6709ccc1896
Dennis Sedov <d@sedov.com>
2022-08-25 09:19:23 -0700

    fix

--- mdserver/views.py
+++ mdserver/views.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Create your views here.
 def prompt(request):
     prompt_value = request.GET['q']
-    img = Image.open('/Users/dennissedov/Library/CloudStorage/GoogleDrive-dennis.sedov@gmail.com/My Drive/sd_output/Zezhou_Chen.png')
+    img = Image.open('somefile.png')
     response = HttpResponse(content_type='image/jpg')
     img.save(response, "JPEG")
     response['Content-Disposition'] = f"attachment; filename=\"{prompt_value}.jpg\""
